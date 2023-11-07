0042461a4032324bedfafb7fc6eb6ea37444591d
Alberto Naranjo <albertonaranjo98@gmail.com>
2023-11-07 02:28:54 +0100

    Add CustomLnF for ComboBox and PopUp menu

--- Source/GUI/Assets/CustomLookAndFeel.h
+++ Source/GUI/Assets/CustomLookAndFeel.h
@@ -15,13 +15,25 @@
 
 using namespace juce;
 
-class CustomLookAndFeel  : public LookAndFeel_V4
+class CustomLookAndFeel : public LookAndFeel_V4
 {
 public:
     CustomLookAndFeel();
     ~CustomLookAndFeel() override;
 
-    virtual void drawButtonText(Graphics& g, TextButton& button, [[maybe_unused]] bool shouldDrawButtonAsHighlighted, [[maybe_unused]] bool shouldDrawButtonAsDown) override;
+    virtual void drawButtonText(Graphics& g, TextButton& button,
+        [[maybe_unused]] bool shouldDrawButtonAsHighlighted, [[maybe_unused]] bool shouldDrawButtonAsDown) override;
+
+    virtual void drawPopupMenuBackground(Graphics& g, int width, int height) override;
+
+    virtual void drawPopupMenuItem(Graphics& g, const Rectangle<int>& area,
+        const bool isSeparator, const bool isActive,
+        const bool isHighlighted, const bool isTicked,
+        const bool hasSubMenu, const String& text,
+        const String& shortcutKeyText,
+        [[maybe_unused]] const Drawable* icon, const Colour* const textColourToUse) override;
+
+    virtual void drawLabel(Graphics& g, Label& label) override;
 
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CustomLookAndFeel)
